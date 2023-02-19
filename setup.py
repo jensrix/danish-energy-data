e34c8645a74256cad980c8a9ebef6b4008c98448
Jens Rix <jensrix@gmail.com>
2023-02-19 20:58:46 +0100

    Initial commit based on cookiecutter-data-science.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project that makes it easy to download and visualize data from energidataservice.dk.',
+    author='Jens Rix',
+    license='MIT',
+)
